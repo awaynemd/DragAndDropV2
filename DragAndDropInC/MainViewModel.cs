061ed9cea86bae70aafd75b8c1155ca07ecb41e7
awaynemd <awaynemd@yahoo.com>
2021-05-19 01:13:01 -0500

    Working version of DragAndDrop in C#

---
+++ DragAndDropInC/MainViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Printing;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace DragAndDropInC
+{
+   
+    public class Printer
+    {
+        public string fullname { get; set; }
+        public string comment { get; set; }
+        public PrintTicket defaultPrintTicket { get; set; }
+        public string description { get; set; }
+        public bool isInError { get; set; }
+        public bool isOffline {get; set; }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region [INotifyPropertyChanged]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // using "virtual" here results in call chain violation CA2214
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        public MainViewModel()
+        {
+            _canExecute = true;
+            Printers = new ObservableCollection<Printer>();
+
+            RefreshPrinterList();
+        }
+
+        public ObservableCollection<Printer> Printers { get; set; }
+
+
+        private ICommand _refreshCommand;
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return _refreshCommand ?? (_refreshCommand = new CommandHandler(() => RefreshPrinterList(), _canExecute));
+            }
+        }
+
+        private bool _canExecute;
+
+        public void RefreshPrinterList()
+        {
+            EnumeratedPrintQueueTypes[] enumerationFlags = {EnumeratedPrintQueueTypes.Local, EnumeratedPrintQueueTypes.Connections};
+
+            var LocalPrintServer  = new PrintServer();           
+            var printQueues = LocalPrintServer.GetPrintQueues(enumerationFlags);
+
+            foreach (PrintQueue p in printQueues)
+            {
+                Printers.Add(new Printer { comment = p.Comment, defaultPrintTicket = p.DefaultPrintTicket, description = p.Description, 
+                                           fullname = p.FullName, isInError = p.IsInError, isOffline = p.IsOffline });
+            }
+
+        }
+
+        public class CommandHandler : ICommand
+        {
+            private Action _action;
+            private bool _canExecute;
+            public CommandHandler(Action action, bool canExecute)
+            {
+                _action = action;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return _canExecute;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+                _action();
+            }
+        }
+    }
+}
---
+++ DragAndDropInC/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragAndDropInC
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            object item = listBox1.SelectedItem;
+            if (item != null)
+                DragDrop.DoDragDrop(listBox1, item, DragDropEffects.Move);
+        }
+
+        private void grid1_Drop(object sender, RoutedEventArgs e)
+        {
+            TextBlock textBlock = e.Source as TextBlock;
+
+            DataObject item = (((DragEventArgs)e).Data) as DataObject;
+            var printer = item.GetData(typeof(Printer)) as Printer;
+            textBlock.Text = printer.fullname;
+        }
+    }   
+}
+
